a1ec00f8e4768be35a241bfa866f44e85964ee68
Dan Weatbrook <weaties@gmail.com>
2015-08-17 04:36:58 +0000

    first attempt that works

--- printer.py
+++ printer.py
@@ -9,10 +9,10 @@ from xhtml2pdf import pisa #for generating PDFs from xhtml
 from subprocess import call #for sending command line items from python
 
 #filename for temp pdf
-filename = "/home/pi/Desktop/Things/Coding/CUPStagram/print.pdf"
+filename = "/home/pi/tmp/tory-print.pdf"
 
-INSTAGRAM_CLIENT_ID='....'
-INSTAGRAM_CLIENT_SECRET='....'
+INSTAGRAM_CLIENT_ID='64cb6ac955094f7da4c0eda3cffdbfba'
+INSTAGRAM_CLIENT_SECRET='9842e540f06240cbbc2326f6687e9aad'
 
 #Start up the API
 api = InstagramAPI(client_id=INSTAGRAM_CLIENT_ID,client_secret=INSTAGRAM_CLIENT_SECRET)
@@ -20,15 +20,19 @@ api = InstagramAPI(client_id=INSTAGRAM_CLIENT_ID,client_secret=INSTAGRAM_CLIENT_
 #Start GUI
 width = 640
 height = 640
-pygame.init()
-window = pygame.display.set_mode((width,height))
+#pygame.init()
+#window = pygame.display.set_mode((width,height))
 
 #Start GPIO
-gpio.setmode(gpio.BCM)
-gpio.setup(25, gpio.OUT)
-gpio.output(25, gpio.HIGH)
-time.sleep(0.5)
-gpio.output(25, gpio.LOW)
+#try:
+#	gpio.setmode(gpio.BCM)
+#	gpio.setup(25, gpio.OUT)
+#	gpio.output(25, gpio.HIGH)
+#	time.sleep(0.5)
+#	gpio.output(25, gpio.LOW)
+
+#except Exception, e:
+#               print e
 
 def showImage(url):
 	p = cStringIO.StringIO(urllib.urlopen(url).read()) #load image url to string, then make it act as a file
@@ -48,7 +52,7 @@ def printout(url):
    		printers = conn.getPrinters()
    		for printer in printers:
     			print printer, printers[printer]["device-uri"]
-     			printer_name = printers.keys()[0] #use first printer in list
+     		printer_name = printers.keys()[0] #use first printer in list
    		conn.printFile(printer_name, filename, "Python_Status_print", {})
    		print("Print request sent")
  	else:
@@ -60,14 +64,20 @@ new_url=''
 old_url=''
 
 while True: #tries again after exception is thrown
+	print "in the while loop"
 	try:
 		while True:
 			#Load media filtered by tag query
-			recent_tag_media = api.tag_recent_media(1,maxID,'thetagyouwantfound')
+			recent_tag_media = api.tag_recent_media(1,maxID,'like4like')
+			print "found a tag" 
+			print recent_tag_media	
 			#get URL and tell it to ignore posts older than it
 			for media in recent_tag_media[0]:
 				new_url = media.images['standard_resolution'].url
+				print "the URL is "
+				print new_url
 				maxID = media.id
+				
 				if (new_url!=old_url) and (len(media.tags)<7): #ignore overtagged spam
 					print "---new post---"
  					print str(media.user)
@@ -75,7 +85,7 @@ while True: #tries again after exception is thrown
  					print "URL: " + str(new_url)
  					#print len(media.tags)
  					print ''
- 					showImage(str(new_url))
+ 					# showImage(str(new_url))
  					printout(str(new_url))
 					old_url = new_url
 		 	#respect rate limit and cpu
