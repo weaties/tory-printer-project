ede4d3e873780665ffe3b6a48b3a67d42c299571
Dan Weatbrook <weaties@gmail.com>
2015-08-17 03:02:19 +0000

    priming the pump - this code is based on
    https://nicshackle.wordpress.com/2014/04/09/hashtag-activated-instagram-printer/

---
+++ printer.py
@@ -0,0 +1,89 @@
+from instagram.client import InstagramAPI
+import time
+import pygame
+import urllib
+import cStringIO
+import RPi.GPIO as gpio
+import cups #for printing
+from xhtml2pdf import pisa #for generating PDFs from xhtml
+from subprocess import call #for sending command line items from python
+
+#filename for temp pdf
+filename = "/home/pi/Desktop/Things/Coding/CUPStagram/print.pdf"
+
+INSTAGRAM_CLIENT_ID='....'
+INSTAGRAM_CLIENT_SECRET='....'
+
+#Start up the API
+api = InstagramAPI(client_id=INSTAGRAM_CLIENT_ID,client_secret=INSTAGRAM_CLIENT_SECRET)
+
+#Start GUI
+width = 640
+height = 640
+pygame.init()
+window = pygame.display.set_mode((width,height))
+
+#Start GPIO
+gpio.setmode(gpio.BCM)
+gpio.setup(25, gpio.OUT)
+gpio.output(25, gpio.HIGH)
+time.sleep(0.5)
+gpio.output(25, gpio.LOW)
+
+def showImage(url):
+	p = cStringIO.StringIO(urllib.urlopen(url).read()) #load image url to string, then make it act as a file
+	background = pygame.image.load(p,"test.jpg") #load image. test.jpg is just a namehint
+	window.blit(background,(0,0)) #put image onto background
+	pygame.display.update()
+
+def printout(url):
+	#generate content
+ 	xhtml = '<h1 style="text-align:center">Your image:</h1>\n'
+	xhtml += '<p style="text-align:center"><img src="'+url+'" align="middle"></p>'
+	pdf = pisa.CreatePDF(xhtml, file(filename, "w"))
+ 	if not pdf.err:
+   		pdf.dest.close()
+   		print("pdf created. Sending print...")
+   		conn = cups.Connection()
+   		printers = conn.getPrinters()
+   		for printer in printers:
+    			print printer, printers[printer]["device-uri"]
+     			printer_name = printers.keys()[0] #use first printer in list
+   		conn.printFile(printer_name, filename, "Python_Status_print", {})
+   		print("Print request sent")
+ 	else:
+   		print("error creating pdf or printing")
+   		xhtml = '' #clear xthml string
+
+maxID=0
+new_url=''
+old_url=''
+
+while True: #tries again after exception is thrown
+	try:
+		while True:
+			#Load media filtered by tag query
+			recent_tag_media = api.tag_recent_media(1,maxID,'thetagyouwantfound')
+			#get URL and tell it to ignore posts older than it
+			for media in recent_tag_media[0]:
+				new_url = media.images['standard_resolution'].url
+				maxID = media.id
+				if (new_url!=old_url) and (len(media.tags)<7): #ignore overtagged spam
+					print "---new post---"
+ 					print str(media.user)
+ 					print str(media.caption)
+ 					print "URL: " + str(new_url)
+ 					#print len(media.tags)
+ 					print ''
+ 					showImage(str(new_url))
+ 					printout(str(new_url))
+					old_url = new_url
+		 	#respect rate limit and cpu
+ 			time.sleep(15)
+
+		#Close window on exit
+		if pygame.QUIT in [e.type for e in pygame.event.get()]:
+			break
+	except Exception, e:
+		print e
+		time.sleep(20) #if failed, wait and try again
